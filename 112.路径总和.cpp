6da74ff507a541eaf2e107e0b1ca1cc7041d0c1a
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-09 22:20:04 +0800

    二叉树 Code112,113

---
+++ 112.路径总和.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ */
+#include<iostream>
+#include<stack>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) 
+    {
+        if(!root)
+            return false;
+        if(root->val == sum && !root->left && !root->right)
+            return true;
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
+// @lc code=end
+int main()
+{
+    TreeNode a(1);
+    TreeNode b(2);
+    a.left = &b;
+    Solution solve;
+    cout << solve.hasPathSum(&a, 1) << endl;
+}
+
---
+++ 113.路径总和-ii.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=cpp
+ *
+ * [113] 路径总和 II
+ */
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> item;
+    vector<vector<int>> result;
+public:
+    void preOrder(TreeNode* root, int target)
+    {
+        if(!root) return;
+        if(!root->left && !root->right)
+        {
+            if(root->val == target)
+            {
+                item.push_back(root->val);
+                result.push_back(item);
+                item.pop_back();
+            }
+            return;
+        }
+        item.push_back(root->val);
+        preOrder(root->left, target - root->val);
+        preOrder(root->right, target - root->val);
+        item.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        preOrder(root, sum);
+        return result;
+    }
+};
+// @lc code=end
+

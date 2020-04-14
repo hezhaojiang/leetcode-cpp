127a163baacfd80cdd0eb563d2a11600d1ae053a
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-14 23:49:14 +0800

    二叉树遍历 Code94 Morris法

--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -23,10 +23,12 @@ struct TreeNode {
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#define MORRIS
 class Solution {
 private:
     vector<int> result;
 public:
+#ifdef RECURSIVE
     void midOrder(TreeNode* root)
     {
         if(!root) return;
@@ -34,10 +36,14 @@ public:
         result.push_back(root->val);
         midOrder(root->right);
     }
+#endif
     vector<int> inorderTraversal(TreeNode* root)
     {
+#ifdef RECURSIVE
         /* 递归 */
         //midOrder(root);
+#endif
+#ifdef ITERATE
         /* 迭代 */
         stack<TreeNode*> node;
         TreeNode* cur = root;
@@ -57,7 +63,57 @@ public:
             }
         }
         return result;
+#endif
+#ifdef MORRIS
+        if(!root) return result;
+        TreeNode* cur = root;
+        while(cur)
+        {
+            if(cur->left)
+            {
+                /* 如果存在左子树，则需要设置索引 */
+                TreeNode* index = cur->left;
+                while(index->right && index->right != cur)
+                {
+                    index = index->right;
+                }
+                
+                if(!index->right)
+                {
+                    /* 首次寻找到索引，设置索引，继续遍历左子树 */
+                    index->right = cur;
+                    cur = cur->left;
+                }
+                else
+                {
+                    /* 非首次寻找到索引，说明左子树遍历完成，根据索引去遍历右子树 */
+                    result.push_back(cur->val);
+                    cur = cur->right;
+                    /* 恢复索引 */
+                    index->right = nullptr;
+                }
+            }
+            else
+            {
+                /* 没有左子树 遍历右子树 */
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return result;
+#endif
     }
 };
 // @lc code=end
+int main()
+{
+    TreeNode a(1);
+    TreeNode b(2);
+    TreeNode c(3);
+    a.right = &b;
+    b.left = &c;
+    Solution solve;
+    solve.inorderTraversal(&a);
+    return 0;
+}
 

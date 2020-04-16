5fcbe8766396332f7c97e2c743bb29392d81a9f4
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-16 23:56:07 +0800

    哈希表 Code1

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -3,12 +3,24 @@
  *
  * [1] 两数之和
  */
-
+#include<vector>
+#include<map>
+#include<string>
+#include<algorithm>
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        map<int, int> mapIi;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mapIi.find(target - nums[i]) != mapIi.end())
+                return vector<int>{i, mapIi[target - nums[i]]};
+            if(mapIi.find(nums[i]) == mapIi.end()) 
+                mapIi[nums[i]] = i;
+        }
+        return vector<int>{0, 0};
     }
 };
 // @lc code=end

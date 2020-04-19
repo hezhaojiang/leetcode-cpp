f452435fef6def84f1c1c88942fbada0a7f24097
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-19 23:18:35 +0800

    动态规划 Code322

--- 322.零钱兑换.cpp
+++ 322.零钱兑换.cpp
@@ -4,36 +4,33 @@
  * [322] 零钱兑换
  */
 #include<vector>
+#include<map>
 #include<algorithm>
 using namespace std;
 // @lc code=start
 class Solution {
+private:
+    vector<int> result;
+    int dp(vector<int>& coins, int amount)
+    {
+        if(amount < 0) return -1;
+        if(amount == 0) return 0;
+        if(result[amount] != 0) return result[amount];
+
+        int method = INT_MAX;
+        for(auto need : coins)
+        {
+            int tempmethod = dp(coins, amount - need);
+            if(tempmethod >= 0 && method > tempmethod + 1)
+                method = tempmethod + 1;
+        }
+        result[amount] = (method == INT_MAX?-1:method);
+        return result[amount];
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
-        if(coins.empty()) return -1;
-        vector<int> dollar;
-        dollar.emplace_back(1);
-        for(int money = 1; money <= amount; money ++)
-        {
-            int method = -1;
-            for(auto need : coins)
-            {
-                
-                if(money - need > 0 && dollar[money - need] != -1)
-                {
-                    if(method == -1) method = dollar[money - need] + 1;
-                    else method = min(method, dollar[money - need] + 1);
-                } 
-                else if(money == need) 
-                {
-                    method = 1;
-                    break;
-                }
-            }
-            dollar.emplace_back(method);
-        }
-        return dollar[amount];
+        result.resize(amount + 1);
+        return dp(coins, amount);
     }
 };
 // @lc code=end

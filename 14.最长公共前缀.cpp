71aad4c51b1a2a399796657c16bb1c004328fb67
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-25 14:51:23 +0800

    Code14

---
+++ 14.最长公共前缀.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=cpp
+ *
+ * [14] 最长公共前缀
+ */
+#include<string>
+#include<vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(!strs.size()) return "";
+        for(int i = 0; i < strs[0].length(); i++)
+        {
+            char c = strs[0][i];
+            for(int j = 1; j < strs.size(); j++)
+                if(i == strs[j].length() || c != strs[j][i]) 
+                    return strs[0].substr(0,i);
+        }
+        return strs[0];
+    }
+};
+// @lc code=end
+

76edc187f94f642b0e06335a7569119f5c0dc0d6
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-27 12:57:34 +0800

    Code13

---
+++ 13.罗马数字转整数.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=cpp
+ *
+ * [13] 罗马数字转整数
+ */
+#include<unordered_map>
+#include<string>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<string, int> m = {{"I", 1}, {"IV", 3}, {"IX", 8}, {"V", 5}, {"X", 10}, {"XL", 30}, {"XC", 80}, {"L", 50}, {"C", 100}, {"CD", 300}, {"CM", 800}, {"D", 500}, {"M", 1000}};
+        int r = m[s.substr(0, 1)];
+        for(int i=1; i<s.size(); ++i){
+            string two = s.substr(i-1, 2);
+            string one = s.substr(i, 1);
+            r += m[two] ? m[two] : m[one];
+        }
+        return r;
+    }
+};
+// @lc code=end
+

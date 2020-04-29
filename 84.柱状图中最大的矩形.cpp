96ffe7f61542c47f24664461d15e18fe83f5906e
He Zhaojiang <hezhaojiang2012@gmail.com>
2020-04-29 21:35:19 +0800

    栈&队列 Code84 ex

--- 84.柱状图中最大的矩形.cpp
+++ 84.柱状图中最大的矩形.cpp
@@ -5,12 +5,14 @@
  */
 #include<vector> 
 #include<algorithm>
+#include<stack>
 #include<unordered_map>
 using namespace std;
 // @lc code=start
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+#ifdef BAOLI
         /* 暴力法 计算每个位置的最大举行面积 */
         int result = 0;
         /* 引入 map 对已经计算过的数据进行记录 防止重复操作 */
@@ -36,6 +38,34 @@ public:
             result = max(result, (rightpos - leftpos + 1) * heights[i]);
         }
         return result;
+#endif
+#ifdef STACK
+        /* 单调栈法 单调递增栈 */
+        int result = 0;
+        stack<int> incStack;
+        int Area = 0;
+        for(int i = 0; i < heights.size(); i++)
+        {
+            while(!incStack.empty() && heights[incStack.top()] > heights[i])
+            {
+                int high = incStack.top();
+                incStack.pop();
+                if(incStack.empty()) Area = heights[high] * i;
+                else Area = heights[high] * (i - incStack.top() - 1);
+                result = max(result, Area);
+            }
+            incStack.push(i);
+        }
+        while(!incStack.empty())
+        {
+            int high = incStack.top();
+            incStack.pop();
+            if(incStack.empty()) Area = heights[high] * heights.size();
+            else Area = heights[high] * (heights.size() - incStack.top() - 1);
+            result = max(result, Area);
+        }
+        return result;
+#endif
     }
 };
 // @lc code=end

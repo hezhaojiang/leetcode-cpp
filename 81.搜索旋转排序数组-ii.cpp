f7df40d820de4498ec185b7ca51f389683e8a500
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-06-13 15:10:21 +0800

    二分法 Code33,81 *重点题

--- 81.搜索旋转排序数组-ii.cpp
+++ 81.搜索旋转排序数组-ii.cpp
@@ -1,22 +1,24 @@
 /*
- * @lc app=leetcode.cn id=33 lang=cpp
+ * @lc app=leetcode.cn id=81 lang=cpp
  *
- * [33] 搜索旋转排序数组
+ * [81] 搜索旋转排序数组 II
  */
 #include<vector>
 using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        if(!nums.size()) return -1;
+    bool search(vector<int>& nums, int target) {
+        if(!nums.size()) return false;
         int start = 0;
         int end = nums.size() - 1;
         int mid = start + (end - start) / 2;
         while(start + 1 < end)
         {
-            if(nums[mid] == target) return mid;
-            else if (nums[mid] >= nums[start]) {
+            if(nums[mid] == target) return true;
+            else if (nums[mid] == nums[start]) start++;
+            else if (nums[mid] == nums[end]) end--;
+            else if (nums[mid] > nums[start]) {
                 if(target >= nums[start] && target < nums[mid])
                     end = mid;
                 else
@@ -30,9 +32,9 @@ public:
             }
             mid = start + (end - start) / 2;
         }
-        if (nums[start] == target) return start;
-        else if (nums[end] == target) return end;
-        return -1;
+        if (nums[start] == target) return true;
+        else if (nums[end] == target) return true;
+        return false;
     }
 };
 // @lc code=end

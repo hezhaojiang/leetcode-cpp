983ae881249dce092fa05da5a002d1a17543995f
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-08-13 22:25:19 +0800

    字符串 Code58

---
+++ 58.最后一个单词的长度.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=cpp
+ *
+ * [58] 最后一个单词的长度
+ */
+#include<string>
+#include<vector>
+#include<regex>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if(s.length() == 0) return 0;
+        std::regex ws_re("\\s+"); // whitespace
+        std::vector<std::string> v(std::sregex_token_iterator(s.begin(), s.end(), ws_re, -1), 
+            std::sregex_token_iterator());
+        int len = 0;
+        for(auto&& s: v)
+            len = s.length();
+        return len;
+    }
+};
+// @lc code=end
+

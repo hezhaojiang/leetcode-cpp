edc63f033da3a485d7f8a8274291d981207a473b
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-08-26 21:23:28 +0800

    深度优先搜索 Code17

--- 17.电话号码的字母组合.cpp
+++ 17.电话号码的字母组合.cpp
@@ -10,18 +10,12 @@ using namespace std;
 // @lc code=start
 class Solution {
 private:
-    map<char, string> tel;
+    vector<string> tel;
     string item;
     vector<string> result;
-    void dfs(string& digits, int index)
-    {
-        if(index >= digits.length())
-        {
-            result.push_back(item);
-            return;
-        }
-        for(auto c : tel[digits[index]])
-        {
+    void dfs(string& digits, int index) {
+        if (index >= digits.length()) return result.push_back(item);
+        for (auto c : tel[digits[index] - '0']) {
             item = item + c;
             dfs(digits, index + 1);
             item = item.substr(0, item.length() - 1);
@@ -30,14 +24,15 @@ private:
     }
 public:
     vector<string> letterCombinations(string digits) {
-        tel['2'] = "abc";
-        tel['3'] = "def";
-        tel['4'] = "ghi";
-        tel['5'] = "jkl";
-        tel['6'] = "mno";
-        tel['7'] = "pqrs";
-        tel['8'] = "tuv";
-        tel['9'] = "wxyz";
+        tel.resize(10);
+        tel[2] = "abc";
+        tel[3] = "def";
+        tel[4] = "ghi";
+        tel[5] = "jkl";
+        tel[6] = "mno";
+        tel[7] = "pqrs";
+        tel[8] = "tuv";
+        tel[9] = "wxyz";
         if(digits.length() == 0) return vector<string>{};
         dfs(digits, 0);
         return result;

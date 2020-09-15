54378409243ef957b98c9f2149799ae39e88e6d1
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-09-15 21:14:43 +0800

    深度优先搜索 Code37 *重点题

---
+++ 37.解数独.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=cpp
+ *
+ * [37] 解数独
+ */
+#include<vector>
+using namespace std;
+// @lc code=start
+class Solution {
+private:
+    int line[9];
+    int column[9];
+    int block[3][3];
+    bool valid;
+    vector<pair<int, int>> spaces;
+
+public:
+    void flip(int i, int j, int digit) {
+        line[i] ^= (1 << digit);
+        column[j] ^= (1 << digit);
+        block[i / 3][j / 3] ^= (1 << digit);
+    }
+
+    void dfs(vector<vector<char>>& board, int pos) {
+        if (pos == spaces.size()) {
+            valid = true;
+            return;
+        }
+
+        auto [i, j] = spaces[pos];
+        int mask = ~(line[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;
+        for (; mask && !valid; mask &= (mask - 1)) {
+            int digitMask = mask & (-mask);
+            int digit = __builtin_ctz(digitMask);
+            flip(i, j, digit);
+            board[i][j] = digit + '0' + 1;
+            dfs(board, pos + 1);
+            flip(i, j, digit);
+        }
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        memset(line, 0, sizeof(line));
+        memset(column, 0, sizeof(column));
+        memset(block, 0, sizeof(block));
+        valid = false;
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    spaces.emplace_back(i, j);
+                }
+                else {
+                    int digit = board[i][j] - '0' - 1;
+                    flip(i, j, digit);
+                }
+            }
+        }
+
+        dfs(board, 0);
+    }
+};
+// @lc code=end
+

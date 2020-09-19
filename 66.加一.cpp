1d06f5c84c547d0c8db21fef148e66bceaa70aba
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-09-19 22:07:08 +0800

    【ADD】数组题目

---
+++ 66.加一.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ */
+#include<vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1]++;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] == 10) { 
+                digits[i] = 0;
+                if (i != 0) digits[i - 1]++;
+                else digits.insert(digits.begin(), 1);
+            }
+        }
+        return digits;
+    }
+};
+// @lc code=end
+

0fb4230ae63b18916c176c0c5a58619034016837
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-09-24 00:46:27 +0800

    【ADD】二叉树遍历

---
+++ 501.二叉搜索树中的众数.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=501 lang=cpp
+ *
+ * [501] 二叉搜索树中的众数
+ */
+#include<vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> result;
+    int last;
+    int count;
+    int maxCount;
+    void dfs(TreeNode* node) {
+        if (!node) return;
+        dfs(node->left);
+        if (count == 0 || last == node->val) count++;
+        else count = 1;
+        last = node->val;
+        if (count == maxCount) result.push_back(last);
+        else if (count > maxCount) {
+            result = vector<int> {last};
+            maxCount = count;
+        }
+        dfs(node->right);
+    }
+public:
+    vector<int> findMode(TreeNode* root) {
+        count = 0;
+        maxCount = 0;
+        dfs(root);
+        return result;
+    }
+};
+// @lc code=end
+
---
+++ 971.翻转二叉树以匹配先序遍历.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=971 lang=cpp
+ *
+ * [971] 翻转二叉树以匹配先序遍历
+ */
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int vsize; 
+    vector<int> result;
+    int dfs(TreeNode* root, vector<int>& voyage, int index) {
+        if (!root || index == vsize) return index - 1;
+        if (root->val != voyage[index]) return -1;
+        if (root->left && root->left->val != voyage[index + 1]) {
+            swap(root->left, root->right);
+            result.push_back(root->val);
+        }
+        index = dfs(root->left, voyage, index + 1);
+        if (index == -1) return -1;
+        index = dfs(root->right, voyage, index + 1);
+        if (index == -1) return -1;
+        return index;
+    }
+public:
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
+        vsize = voyage.size();
+        if (-1 == dfs(root, voyage, 0)) return vector<int>{-1};
+        return result;
+    }
+};
+// @lc code=end
+

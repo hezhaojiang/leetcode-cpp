876adafcf78298e555a4d78a5e26a47b0c406ac2
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-10-09 23:38:47 +0800

    【MOD】统一代码风格 双指针问题

--- 344.反转字符串.cpp
+++ 344.反转字符串.cpp
@@ -9,8 +9,7 @@ using namespace std;
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for(int i = 0, j = s.size() - 1; i < j; i++, j--)
-            swap(s[i], s[j]);
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--) swap(s[i++], s[j--]);
         return;
     }
 };
--- 75.颜色分类.cpp
+++ 75.颜色分类.cpp
@@ -11,8 +11,7 @@ public:
     void sortColors(vector<int>& nums) {
         int left = 0;
         int right = nums.size() - 1;
-        for(int i = 0; i <= right;)
-        {
+        for(int i = 0; i <= right;) {
             if(nums[i] == 0) swap(nums[i++], nums[left++]);
             else if(nums[i] == 2) swap(nums[i], nums[right--]);
             else i++;

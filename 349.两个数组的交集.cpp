7f0254ff51d86f09efb55be1fa2aa766fad8015d
He Zhaojiang <hezhaojiang2012@foxmail.com>
2020-11-04 00:19:48 +0800

    【MOD】统一代码风格 每日一题

--- 349.两个数组的交集.cpp
+++ 349.两个数组的交集.cpp
@@ -15,32 +15,16 @@ public:
         vector<int>* l;
         unordered_map<int, int> Hashnum;
         vector<int> result;
-        if(nums1.size() < nums2.size()) 
-        {
-            s = &nums1;
-            l = &nums2;
-        }
-        else
-        {
-            s = &nums1;
-            l = &nums2;
-        }
+        if(nums1.size() < nums2.size()) { s = &nums1; l = &nums2; }
+        else { s = &nums1; l = &nums2; }
 
-        for(auto i : *s)
-        {
-            Hashnum[i] = 1;
-        }
-
-        for(auto i : *l)
-        {
-            if(Hashnum.find(i) != Hashnum.end())
-            {
+        for(auto i : *s) Hashnum[i] = 1;
+        for(auto i : *l) {
+            if(Hashnum.find(i) != Hashnum.end()) {
                 result.emplace_back(i);
                 Hashnum.erase(i);
             }
-                
         }
-        
         return result;
     }
 };

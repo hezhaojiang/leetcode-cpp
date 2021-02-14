6f83ca72ad358b13b9eb134224b3ae417ddf0e64
HeZhaoJiang <hezhaojiang2012@foxmail.com>
2021-02-14 22:57:06 +0800

    【ADD】广度优先搜索

---
+++ 126.单词接龙-ii.cpp
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=126 lang=cpp
+ *
+ * [126] 单词接龙 II
+ */
+#include<vector>
+#include<string>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+// @lc code=start
+class Solution {
+private:
+    bool transable(vector<string>& wordList, int i, int j) {
+        int res = 0;
+        for (int n = 0; n < wordList[i].size(); n++) {
+            if (wordList[i][n] != wordList[j][n]) res++;
+        }
+        return res == 1;
+    }
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, int> word2index;
+        int N = wordList.size();
+        for (int i = 0; i < N; i++) word2index[wordList[i]] = i;
+        if (!word2index.count(endWord)) return {};
+        if (!word2index.count(beginWord)) {
+            wordList.push_back(beginWord);
+            word2index[beginWord] = N;
+        }
+        N = wordList.size();
+
+        // 预处理 建图
+        vector<vector<int>> graph(N, vector<int>{});
+        for (int i = 0; i < N; i++) for (int j = 0; j < i; j++) {
+            if (transable(wordList, i, j)) {
+                graph[i].push_back(j);
+                graph[j].push_back(i);
+            }
+        }
+
+        // 广度优先搜索
+        bool bcontinue = true;
+        queue<vector<int>> Q;
+        vector<int> visited(N, 0);
+        vector<int> cost(N, INT_MAX); // 这个 cost 记录 beginword 到所有点的最短距离
+        const int dest = word2index[endWord];
+        vector<vector<string>> res;
+        Q.push(vector<int>{word2index[beginWord]});
+        visited[word2index[beginWord]] = 1;
+        cost[word2index[beginWord]] = 0;
+
+        while (!Q.empty() && bcontinue) {
+            unordered_map<int, int> curvisit;
+            int currSize = Q.size();
+            for (int i = 0; i < currSize; i++) {
+                vector<int> now = Q.front();
+                Q.pop();
+                int index = now.back();
+                if (dest == index) {
+                    vector<string> tmp;
+                    for (int index : now) tmp.push_back(wordList[index]);
+                    res.push_back(tmp);
+                    bcontinue = false;
+                } else {
+                    for (int j = 0; j < graph[index].size(); j++) {
+                        if (visited[graph[index][j]] == 0) {
+                            vector<int> copy(now);
+                            copy.push_back(graph[index][j]);
+                            Q.push(copy);
+                            curvisit[graph[index][j]] = 1;
+                        }
+                    }
+                }
+            }
+            for (auto & v : curvisit) visited[v.first] = 1;
+        }
+        return res;
+    }
+};
+// @lc code=end
+

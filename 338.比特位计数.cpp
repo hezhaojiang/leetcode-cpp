604e3875cf71ab61ccd4176ae11ff52b2a512d34
HeZhaoJiang <hezhaojiang2012@foxmail.com>
2021-03-03 12:30:36 +0800

    【MOD】位运算 每日一题

--- 338.比特位计数.cpp
+++ 338.比特位计数.cpp
@@ -8,29 +8,10 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-#if 0
-    int hammingWeight(uint32_t n) {
-        if (n == 0) return 0;
-        return hammingWeight(n & (n - 1)) + 1;
-    }
     vector<int> countBits(int num) {
-        vector<int> result;
-        for (int i = 0; i <= num; i++) {
-            result.push_back(hammingWeight(i));
-        }
-        return result;
-    }
-#endif
-    vector<int> countBits(int num) {
-        vector<int> result{0};
-        while (result.size() <= num) {
-            int N = result.size();
-            if (2 * N > num) N = num - N + 1;
-            for (int i = 0; i < N; i++) {
-                result.push_back(result[i] + 1);
-            }
-        }
-        return result;
+        vector<int> res(num + 1, 0);
+        for (int i = 1; i <= num; i++) res[i] = res[i >> 1] + (i & 1);
+        return res;
     }
 };
 // @lc code=end
